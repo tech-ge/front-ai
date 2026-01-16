fb8e2ca2514c54b46c6e2347c84c36df71fa02e2
Tech <tech@example.com>
2026-01-16 19:07:04 +0300

    Disable CSS minification completely and remove all opacity modifiers from CSS

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    optimizePackageImports: ['@'],
+  swcMinify: false,
+  webpack: (config) => {
+    config.optimization.minimize = false
+    return config
+  },
+  onDemandEntries: {
+    maxInactiveAge: 60 * 1000,
+    pagesBufferLength: 5,
   },
 }
 
